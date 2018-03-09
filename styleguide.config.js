eaab0c14d8a0943e26c9556cf070a61858c45f7f
mehanig <mehanig@gmail.com>
2018-03-09 23:24:20 +0300

    better config

--- styleguide.config.js
+++ styleguide.config.js
@@ -1,16 +1,32 @@
 const { createConfig, babel, postcss } = require('webpack-blocks');
 const typescript = require('@webpack-blocks/typescript')
+const path = require('path');
 
 const propsParser = function (filePath, source, resolver, handlers) {
-    return require('react-docgen-typescript').withCustomConfig('./tsconfig.json');
+  return require('react-docgen-typescript').withCustomConfig('./tsconfig.json');
 }
 
 module.exports = {
-    propsParser: require('react-docgen-typescript').withCustomConfig('./tsconfig.json').parse,
-    components: 'src/**/*.tsx',
-    webpackConfig: createConfig([
-        babel(),
-        postcss(),
-        typescript()
-    ])
+  showSidebar: true,
+  theme: {
+    color: {
+      baseBackground: '#303030',
+      sidebarBackground: '#404040',
+      codeBackground: '#a1a1a1',
+      type: '#909090',
+      base: '#666',
+      light: '#999',
+      lightest: '#ccc',
+    },
+    fontFamily: {
+      base: "Helvetica Neue"
+    }
+  },
+  propsParser: require('react-docgen-typescript').withCustomConfig('./tsconfig.json').parse,
+  components: 'src/**/*.tsx',
+  webpackConfig: createConfig([
+    babel(),
+    postcss(),
+    typescript()
+  ])
 };
